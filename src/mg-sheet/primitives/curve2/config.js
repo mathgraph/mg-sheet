54549627a75729b15285af3dc3607c572f6224eb
Maks <maks44ru@yandex.ru>
2015-11-16 18:39:49 +0300

    fix(curve2)

--- src/mg-sheet/primitives/curve2/config.js
+++ src/mg-sheet/primitives/curve2/config.js
@@ -1,6 +1,5 @@
 define(function () {
     return {
-        deltaZero: 0.0001,
         step: 1
     }
 });
--- src/mg-sheet/primitives/curve2/curve2.js
+++ src/mg-sheet/primitives/curve2/curve2.js
@@ -10,13 +10,13 @@ define(['mg-sheet/utils/common', './config'], function (utils, defaultConfig) {
         ae = a;
         be = c * y + d;
         ce = b * y * y + e * y + f;
-        if (Math.abs(a) <= defaultConfig.deltaZero) {
+        if (Math.abs(a) <= 0) {
             return [-(ce) / (be)];
         } else {
             D = Math.pow(be, 2) - 4 * ae * (ce);
-            if (D > defaultConfig.deltaZero) {
+            if (D > 0) {
                 return [(-(be) + Math.sqrt(D)) / (2 * ae), (-(be) - Math.sqrt(D)) / (2 * ae)];
-            } else if (Math.abs(D) < defaultConfig.deltaZero) {
+            } else if (Math.abs(D) < 0) {
                 return [(-(be)) / (2 * ae)];
             }
         }
@@ -29,7 +29,7 @@ define(['mg-sheet/utils/common', './config'], function (utils, defaultConfig) {
     return {
         type: 'primitive',
         factory: function draw_curve2(coefficients, style) {
-            var sheet, path, getSegments, getArray,
+            var sheet, path, getSegments, getArray, connectSegments,
                 leftBorderDrawing, rightBorderDrawing, topBorderDrawing, bottomBorderDrawing;
             sheet = this;
             leftBorderDrawing = (sheet.center[0] - sheet.width / 2);
@@ -37,83 +37,90 @@ define(['mg-sheet/utils/common', './config'], function (utils, defaultConfig) {
             topBorderDrawing = (sheet.center[1] + sheet.height / 2);
             bottomBorderDrawing = (sheet.center[1] - sheet.height / 2);
 
+            connectSegments = function (left, right, bottom, top, s1, s2) {
+                var previousPoint;
+                if (s1.length != 0 && s2.length != 0) {
+                    previousPoint = s1[s1.length - 1];
+                    if (previousPoint[0] >= right - 5 * defaultConfig.step || previousPoint[1] >= top - 5 * defaultConfig.step) {
+                        s1.push([right, top]);
+                        if (s2[0][0] <= left + 5 * defaultConfig.step || s2[0][1] <= bottom + 5 * defaultConfig.step) {
+                            s1.push([right, bottom]);
+                            s1.push([left, bottom]);
+                        }
+                    } else if (previousPoint[0] <= left + 5 * defaultConfig.step || previousPoint[1] <= bottom + 5 * defaultConfig.step) {
+                        s1.push([left, bottom]);
+                        if (s2[0][0] >= right - 5 * defaultConfig.step || s2[0][1] >= top - 5 * defaultConfig.step) {
+                            s1.push([left, top]);
+                            s1.push([right, top]);
+                        }
+                    }
+                }
+                return s1.concat(s2);
+            };
 
-            getArray = function (begin, end, a, b, c, d, e, f) {
-                var points, points1, points2, i, prePointsIsExist, prePointsIsVisible,
-                    previousPoint, res, prePoint1, prePoint2;
+            getArray = function (left, right, bottom, top, a, b, c, d, e, f) {
+                var points, points1, points2, i, prePointsIsExist, prePointsIsVisible1, prePointsIsVisible2,
+                    res, prePoint1, prePoint2;
                 points = [];
                 points1 = [];
                 points2 = [];
                 prePointsIsExist = false;
-                prePointsIsVisible = false;
+                prePointsIsVisible1 = false;
+                prePointsIsVisible2 = false;
                 prePoint1 = null;
                 prePoint2 = null;
 
-                for (i = begin; i <= end; i += defaultConfig.step) {
-                    res = calculateX(i, a, b, c, d, e, f);
+                for (i = left; i <= right; i += defaultConfig.step) {
+                    res = calculateY(i, a, b, c, d, e, f);
                     if (res.length > 0) {
-                        if (res[0] < rightBorderDrawing && res[0] > leftBorderDrawing) {
-                            !prePointsIsVisible && prePoint1 != null && points1.push([prePoint1, i - defaultConfig.step]);
-                            points1.push([res[0], i]);
-                            prePointsIsVisible = true;
+                        if (res[0] < top && res[0] > bottom) {
+                            !prePointsIsVisible1 && prePoint1 != null && points1.push([i - defaultConfig.step, prePoint1]);
+                            points1.push([i, res[0]]);
+                            prePointsIsVisible1 = true;
                         } else {
-                            prePointsIsVisible && points1.push([res[0], i]);
-                            prePointsIsVisible = false;
+                            prePoint1 = res[0];
+                            prePointsIsVisible1 && points1.push([i, res[0]]);
+                            prePointsIsVisible1 = false;
                         }
                         if (res.length === 2) {
-                            if (res[1] < rightBorderDrawing && res[1] > leftBorderDrawing) {
-                                !prePointsIsVisible && prePoint2 != null && points2.push([prePoint2, i - defaultConfig.step]);
-                                points2.push([res[1], i]);
-                                prePointsIsVisible = true;
+                            if (res[1] < top && res[1] > bottom) {
+                                !prePointsIsVisible2 && prePoint2 != null && points2.push([i - defaultConfig.step, prePoint2]);
+                                points2.push([i, res[1]]);
+                                prePointsIsVisible2 = true;
                             } else {
-                                prePointsIsVisible && points1.push([res[0], i]);
-                                prePointsIsVisible = false;
+                                prePoint2 = res[1];
+                                prePointsIsVisible2 && points1.push([i, res[0]]);
+                                prePointsIsVisible2 = false;
                             }
                         }
                         prePointsIsExist = true;
                     } else {
                         if (prePointsIsExist) {
                             points = points.concat(points1.concat(points2.reverse()));
-                            points.push(points1[0]);
+                            points = connectSegments(left, right, bottom, top, points, [points1[0].slice()]);
                             points1 = [];
                             points2 = [];
                         }
                         prePointsIsExist = false;
                     }
                 }
-                if (/*prePointsIsExist && */(points1.length !== 0 || points2.length !== 0)) {
-                    points1 = points1.concat(points2.reverse());
-                    if (points.length != 0) {
-                        previousPoint = points[points.length - 1];
-                        if (previousPoint[0] >= rightBorderDrawing || previousPoint[1] >= topBorderDrawing) {
-                            points.push([rightBorderDrawing, topBorderDrawing]);
-                            if (points1[0][0] <= leftBorderDrawing || points1[0][1] <= bottomBorderDrawing) {
-                                points.push([rightBorderDrawing, bottomBorderDrawing]);
-                                points.push([leftBorderDrawing, bottomBorderDrawing]);
-                            }
-                        } else if (previousPoint[0] <= leftBorderDrawing || previousPoint[1] <= bottomBorderDrawing) {
-                            points.push([leftBorderDrawing, bottomBorderDrawing]);
-                            if (points1[0][0] >= rightBorderDrawing || points1[0][1] >= topBorderDrawing) {
-                                points.push([leftBorderDrawing, topBorderDrawing]);
-                                points.push([rightBorderDrawing, topBorderDrawing]);
-                            }
-                        }
-                    }
-                    points = points.concat(points1);
-                }
+                points1 = connectSegments(left, right, bottom, top, points2.reverse(), points1);
+                points = connectSegments(left, right, bottom, top, points, points1);
                 return points;
             };
             getSegments = function (coeff) {
                 var res = getArray(bottomBorderDrawing - 5 * defaultConfig.step, topBorderDrawing + 5 * defaultConfig.step,
-                    coeff.A, coeff.B, coeff.C, coeff.D, coeff.E, coeff.F);
-                if (res.length < 5) {
+                    leftBorderDrawing - 5 * defaultConfig.step, rightBorderDrawing + 5 * defaultConfig.step,
+                    coeff.B, coeff.A, coeff.C, coeff.E, coeff.D, coeff.F);
+                res.forEach(function (p) {
+                    var k = p[0];
+                    p[0] = p[1];
+                    p[1] = k;
+                });
+                if (res.length < 15) {
                     res = getArray(leftBorderDrawing - 5 * defaultConfig.step, rightBorderDrawing + 5 * defaultConfig.step,
-                        coeff.B, coeff.A, coeff.C, coeff.E, coeff.D, coeff.F);
-                    res.forEach(function (p) {
-                        var k = p[0];
-                        p[0] = p[1];
-                        p[1] = k;
-                    });
+                        bottomBorderDrawing - 5 * defaultConfig.step, topBorderDrawing + 5 * defaultConfig.step,
+                        coeff.A, coeff.B, coeff.C, coeff.D, coeff.E, coeff.F);
                 }
                 return res;
             };
